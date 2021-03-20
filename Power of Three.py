c300b0abd42d0eedbe80943427c5269182437721
priyank-python <78577278+priyank-python@users.noreply.github.com>
2021-03-19 22:27:33 -0500

    Add files via upload

---
+++ Power of Three.py
@@ -0,0 +1,50 @@
+'''
+Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+ 
+
+Example 1:
+
+Input: n = 27
+Output: true
+Example 2:
+
+Input: n = 0
+Output: false
+Example 3:
+
+Input: n = 9
+Output: true
+Example 4:
+
+Input: n = 45
+Output: false
+ 
+
+Constraints:
+
+-231 <= n <= 231 - 1
+'''
+
+import math
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+       
+        if n < 1:
+            return False
+        
+        x = 0
+        limit = math.ceil (math.log(n,3))
+        
+        while x <= limit:
+            if 3**x == n:
+                return True
+            x = x + 1
+            
+        return False
